fc820ff80f4db1419752244cb9725cfa1ba4f331
Jon Samp <sampjon@gmail.com>
2023-02-06 23:14:11 -0600

    use base and asset prefixes

--- docs/next.config.js
+++ docs/next.config.js
@@ -130,4 +130,6 @@ export default {
     const cacheHeaders = [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }];
     return [{ source: '/_next/static/:static*', headers: cacheHeaders }];
   },
+  basePath: process.env.AWS_BUCKET,
+  assetPrefix: process.env.AWS_BUCKET,
 };
